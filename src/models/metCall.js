cb03dbe7c1aa0f2ae80afbc8b2887d03dc12df8a
tjw <tjw@tjw-ubuntu-24.10-ssd>
2025-09-23 18:38:17 +0200

    add additional requests to get artworks data but no testing yet

--- src/models/metCall.js
+++ src/models/metCall.js
@@ -4,6 +4,18 @@ exports.fetchMetArtworks = (query) => {
   const url = `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${query}`;
 
   return axios.get(url).then(({ data }) => {
-    return data;
-  });
+    const first20 = data.objectIDs.slice(0, 20);
+
+    const artworksRequest = first20.map((id) => {
+      return axios
+        .get(
+          `https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`
+        )
+        .then(({ data }) => data);
+    });
+
+    return Promise.all(artworksRequest)
+  }).then((artworks)=>{
+    return artworks
+  })
 };
